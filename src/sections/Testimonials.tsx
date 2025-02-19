a68ba6b33424eedb080fa7122bfab5340b9ad6ce
Saeedul <karimsaeedul476@gmail.com>
2025-02-19 22:13:03 +0600

    updated testimonials section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -6,6 +6,7 @@ import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 import { SectionHeader } from "@/components/SectionHeader";
 import Image from "next/image";
 import grainImage from "@/assets/images/grain.jpg";
+import { Card } from "@/components/Card";
 
 const testimonials = [
   {
@@ -41,23 +42,34 @@ const testimonials = [
 ];
 
 export const TestimonialsSection = () => {
-  return <div className="py-16">
+  return <div className="py-16 lg:py-24">
     <div className="container">
       <SectionHeader eyebrow="Happy Clients" title="What Clients Say About Me" description="Don't just take my word for it. See what my clients have to say about my work." />
-      <div>
-        {testimonials.map(testimonial => (
-          <div key={testimonial.name} className="bg-gray-800 rounded-3xl p-6 relative overflow-hidden">
-            <div className="absolute inset-0" style={{
-              backgroundImage: `url(${grainImage.src})`,
-            }}>
-            </div>
-            <Image src={testimonial.avatar} alt={testimonial.name} />
-            <div>{testimonial.name}</div>
-            <div>{testimonial.position}</div>
-            <p>{testimonial.text}</p>
-          </div>
-        ))}
+      <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+        {/* Here the mask-image property creates a linear gradient mask that:
+          Starts as fully transparent on the left.
+          Gradually becomes fully visible (black) at 10% of the width.
+          Stays fully visible between 10% and 90%.
+          Gradually fades to fully transparent at 90% until the right edge. */}
+        <div className="flex gap-8 flex-none">
+          {testimonials.map(testimonial => (
+            <Card key={testimonial.name} className="max-w-xs md:max-w-md md:p-8">
+              <div className="flex gap-4 items-center">
+                <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                  <Image src={testimonial.avatar}
+                    alt={testimonial.name}
+                    className="max-h-full" />
+                </div>
+                <div>
+                  <div className="font-semibold">{testimonial.name}</div>
+                  <div className="text-sm text-white/40">{testimonial.position}</div>
+                </div>
+              </div>
+              <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
-  </div>;
+  </div>
 };
