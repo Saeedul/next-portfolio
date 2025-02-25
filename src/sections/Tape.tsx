8bdea32d5fe4e284e1e7644ad28480a9d17a5b6d
Saeedul <karimsaeedul476@gmail.com>
2025-02-25 21:20:40 +0600

    added tapes and testimonial animations

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,4 +1,5 @@
 import StarIcon from '@/assets/icons/star.svg';
+import { Fragment } from 'react';
 
 const words = [
   "Performant",
@@ -18,12 +19,18 @@ export const TapeSection = () => {
   return <div className="py-16 lg:py-24 overflow-x-clip">
     <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
       <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-        <div className="flex flex-none gap-4 py-3">
-          {words.map(word => (
-            <div key={word} className="inline-flex gap-4 items-center">
-              <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span>
-              <StarIcon className="size-6 text-gray-900 -rotate-12" />
-            </div>
+        <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:30s]">
+          {/* here, we're creating an array of two values, filling it with a dummy value of 0 and then mapping over it. Because were going to have to duplicate the words in side the tape so that it does not end when the animation goes on. */}
+          {[...new Array(2)].fill(0).map((_, idx) => (
+            // we wrap the word mappings inside a react fragment in order to avoid react errors and keep it happy.
+            <Fragment key={idx}>
+              {words.map(word => (
+                <div key={word} className="inline-flex gap-4 items-center">
+                  <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span>
+                  <StarIcon className="size-6 text-gray-900 -rotate-12" />
+                </div>
+              ))}
+            </Fragment>
           ))}
         </div>
       </div>
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -7,6 +7,7 @@ import { SectionHeader } from "@/components/SectionHeader";
 import Image from "next/image";
 import grainImage from "@/assets/images/grain.jpg";
 import { Card } from "@/components/Card";
+import { Fragment } from "react";
 
 const testimonials = [
   {
@@ -45,28 +46,34 @@ export const TestimonialsSection = () => {
   return <div className="py-16 lg:py-24">
     <div className="container">
       <SectionHeader eyebrow="Happy Clients" title="What Clients Say About Me" description="Don't just take my word for it. See what my clients have to say about my work." />
-      <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+      {/* we used py-4 to make the nexessary padding for the testimonial card to appear in full when we hover over it. But as this added more padding, we adjusted the margin by changing the others to mt-12 lg:mt-20 and adding -my-4. This helps us to view the rotated card in full when we hover it without changing the given padding. */}
+      <div className="mt-12 lg:mt-20 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 -my-4">
         {/* Here the mask-image property creates a linear gradient mask that:
           Starts as fully transparent on the left.
           Gradually becomes fully visible (black) at 10% of the width.
           Stays fully visible between 10% and 90%.
           Gradually fades to fully transparent at 90% until the right edge. */}
-        <div className="flex gap-8 flex-none">
-          {testimonials.map(testimonial => (
-            <Card key={testimonial.name} className="max-w-xs md:max-w-md p-6 md:p-8">
-              <div className="flex gap-4 items-center">
-                <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
-                  <Image src={testimonial.avatar}
-                    alt={testimonial.name}
-                    className="max-h-full" />
-                </div>
-                <div>
-                  <div className="font-semibold">{testimonial.name}</div>
-                  <div className="text-sm text-white/40">{testimonial.position}</div>
-                </div>
-              </div>
-              <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
-            </Card>
+        <div className="flex gap-8 pr-8 flex-none animate-move-left [animation-duration:90s] hover:[animation-play-state:paused]">
+          {/* Here we need to duplicate the array as the words will be mapped in order to show an infinite scroll sideways animation while rotating. So we created a new array with only two values, fill them with a dummy value which is 0. And then we're going to map over those two values and go ahead and get the index of that particular item. So, now instead of of one set of testimonial cards, we have two sets because we just mapped an array on length 2. The index will be 0 and 1. And for each one of those mapped items we create a set of testimonials. We have two sets instead of just 1. This will be helpful once we translate left just like we did in the tapes section.*/}
+          {[...new Array(2)].fill(0).map((_, index) => (
+            <Fragment key={index}>
+              {testimonials.map(testimonial => (
+                <Card key={testimonial.name} className="max-w-xs md:max-w-md p-6 md:p-8 hover:-rotate-3 transition duration-300">
+                  <div className="flex gap-4 items-center">
+                    <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                      <Image src={testimonial.avatar}
+                        alt={testimonial.name}
+                        className="max-h-full" />
+                    </div>
+                    <div>
+                      <div className="font-semibold">{testimonial.name}</div>
+                      <div className="text-sm text-white/40">{testimonial.position}</div>
+                    </div>
+                  </div>
+                  <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
+                </Card>
+              ))}
+            </Fragment>
           ))}
         </div>
       </div>
